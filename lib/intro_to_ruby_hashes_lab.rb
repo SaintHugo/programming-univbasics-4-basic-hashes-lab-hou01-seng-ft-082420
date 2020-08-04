d3789147b11548f174eb0cdd0199a3f0ff39a32a
Hugo Rodrigez <hugor37@gmail.com>
2020-08-04 20:02:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-{;goat => "Mac Miller"}  # return a valid hash with any key/value pair of your choice
+{:goat => "Mac Miller"}  # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
